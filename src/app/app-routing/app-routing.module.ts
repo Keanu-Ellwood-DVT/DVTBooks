b5ef361bf5ea4b46185c0068746b9a3a8e4f6034
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-22 14:36:40 +0200

    Add app-routing module

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from '../profile/profile.component';
+import { AuthGuard } from '../auth.guard';
+
+    const routes: Routes = [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [AuthGuard]
+      }
+    ];
+
+    @NgModule({
+        imports: [
+            RouterModule.forRoot(routes)
+        ],
+        exports: [
+            RouterModule
+        ],
+        declarations: []
+    })
+    export class AppRoutingModule { }
