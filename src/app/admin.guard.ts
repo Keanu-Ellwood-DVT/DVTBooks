d5dec0addb8f78d5f7dc2d40db16558bfadd3d6b
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-24 16:13:55 +0200

    Add admin guard

---
+++ src/app/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+
+    //let isAdmin : boolean = this.auth._checkAdmin(this.auth.userProfile$);
+    if (this.auth.isAdmin) {
+      return true;
+    }
+    this.router.navigate(['/forbidden']);
+    return false;
+  }
+
+}
