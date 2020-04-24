ed16e89f9590ea27e4bdcf2076b67154c49c1677
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-24 16:15:49 +0200

    Update modules and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminDeleteComponent } from './admin/admin-delete/admin-delete.component';
+import { AdminUpdateComponent } from './admin/admin-update/admin-update.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ProfileComponent
+    ProfileComponent,
+    ForbiddenComponent,
+    AdminComponent,
+    AdminDeleteComponent,
+    AdminUpdateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
