cd028b62fc6560bc43020a303a41b793177e6edd
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-28 11:55:12 +0200

    Remove comments and unused methods

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,76 +4,60 @@ import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
 import { from, of, Observable, BehaviorSubject, combineLatest, throwError } from 'rxjs';
 import { tap, catchError, concatMap, shareReplay, debounceTime } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { AUTH_CONFIG } from './auth.config';
+import { UserProfile } from 'src/models/user-profile';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // Create an observable of Auth0 instance of client
+
   auth0Client$ = (from(
     createAuth0Client({
-      domain: 'dvt-web-grad.auth0.com',
-      client_id: 'S0t2pnTp9SSrKg4JcOLmL981NF2z22wt',
+      domain: environment.domain,
+      client_id: environment.client_id,
       redirect_uri: `${window.location.origin}`
     })
   ) as Observable<Auth0Client>).pipe(
-    shareReplay(1), // Every subscription receives the same shared value
+    shareReplay(1),
     catchError(err => throwError(err))
   );
-  // Define observables for SDK methods that return promises by default
-  // For each Auth0 SDK method, first ensure the client instance is ready
-  // concatMap: Using the client instance, call SDK method; SDK returns a promise
-  // from: Convert that resulting promise into an observable
+
   isAuthenticated$ = this.auth0Client$.pipe(
     concatMap((client: Auth0Client) => from(client.isAuthenticated())),
     tap(res => this.loggedIn = res)
   );
+
   handleRedirectCallback$ = this.auth0Client$.pipe(
     concatMap((client: Auth0Client) => from(client.handleRedirectCallback()))
   );
-  // Create subject and public observable of user profile data
+
   private userProfileSubject$ = new BehaviorSubject<any>(null);
   userProfile$ = this.userProfileSubject$.asObservable();
-  // Create a local property for login status
+
   loggedIn: boolean = null;
-  private userProfileData = {};
+  userProfileData: UserProfile = null;
+  private userRoles: string[] = [];
   isAdmin: boolean = null;
   constructor(private router: Router) {
-    // On initial load, check authentication state with authorization server
-    // Set up local auth streams if user is already authenticated
     this.localAuthSetup();
-    // Handle redirect from Auth0 login
     this.handleAuthCallback();
-    // this.userProfile$.subscribe(data => console.log(data));
   }
 
-  // When calling, options can be passed if desired
-  // https://auth0.github.io/auth0-spa-js/classes/auth0client.html#getuser
   getUser$(options?): Observable<any> {
     return this.auth0Client$.pipe(
       concatMap((client: Auth0Client) => from(client.getUser(options))),
-      tap(user => this.userProfileSubject$.next(user))
+      tap(user => {this.userProfileSubject$.next(user),
+        this.userProfileData = user})
     );
   }
 
   private localAuthSetup() {
-    // This should only be called on app initialization
-    // Set up local authentication streams
     const checkAuth$ = this.isAuthenticated$.pipe(
       concatMap((loggedIn: boolean) => {
         if (loggedIn) {
-          // If authenticated, get user and set in app
-          // NOTE: you could pass options here if needed
-
-          // this.userProfile$.subscribe(data => this.userProfileData = data);
-          // console.log(this.userProfileData);
-          // this.isAdmin = this._checkAdmin(this.userProfileData);
-          // console.log("isAdmin: " + this.isAdmin);
-
           return this.getUser$();
         }
-        // If not authenticated, return stream that emits 'false'
         return of(loggedIn);
       })
     );
@@ -81,11 +65,7 @@ export class AuthService {
   }
 
   login(redirectPath: string = '/') {
-    // A desired redirect path can be passed to login method
-    // (e.g., from a route guard)
-    // Ensure Auth0 client instance exists
     this.auth0Client$.subscribe((client: Auth0Client) => {
-      // Call method to log in
       client.loginWithRedirect({
         redirect_uri: `${window.location.origin}`,
         appState: { target: redirectPath }
@@ -94,53 +74,29 @@ export class AuthService {
   }
 
   private handleAuthCallback() {
-    // Call when app reloads after user logs in with Auth0
     const params = window.location.search;
     if (params.includes('code=') && params.includes('state=')) {
-      let targetRoute: string; // Path to redirect to after login processed
+      let targetRoute: string;
       const authComplete$ = this.handleRedirectCallback$.pipe(
-        // Have client, now call method to handle auth callback redirect
         tap(cbRes => {
-          // Get and set target redirect route from callback results
           targetRoute = cbRes.appState && cbRes.appState.target ? cbRes.appState.target : '/';
         }),
         concatMap(() => {
-          // Redirect callback complete; get user and login status
           return combineLatest([
             this.getUser$(),
             this.isAuthenticated$
           ]);
         })
       );
-      // Subscribe to authentication completion observable
-      // Response will be an array of user and login status
+
       authComplete$.subscribe(([user, loggedIn]) => {
-        // Redirect to target route after callback processing
         this.router.navigate([targetRoute]);
       });
     }
   }
 
-  _checkAdmin(profile): boolean {
-
-    // let profile = this.userProfile$.pipe(debounceTime(500)).subscribe(data => this.userProfileData = data);
-    // console.log(profile);
-    // this.isAdmin = this._checkAdmin(this.userProfileData);
-    // console.log("isAdmin: " + this.isAdmin);
-
-    const roles = profile[AUTH_CONFIG.NAMESPACE] || [];
-
-    if (roles.indexOf('Admin') > -1 ) {
-      return true;
-    }
-
-    return false;
-  }
-
   logout() {
-    // Ensure Auth0 client instance exists
     this.auth0Client$.subscribe((client: Auth0Client) => {
-      // Call method to log out
       client.logout({
         client_id: 'S0t2pnTp9SSrKg4JcOLmL981NF2z22wt',
         returnTo: `${window.location.origin}`
