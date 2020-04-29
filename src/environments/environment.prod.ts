928a605a4dc96bd98c9fcccccfd43c27cb28bc75
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-29 14:06:24 +0200

    Add apiUri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   NAMESPACE: 'http://localhost:4200/role',
   domain: 'dvt-web-grad.auth0.com',
-  client_id: 'S0t2pnTp9SSrKg4JcOLmL981NF2z22wt'
+  client_id: 'S0t2pnTp9SSrKg4JcOLmL981NF2z22wt',
+  apiUri: 'http://localhost:4201'
 };
