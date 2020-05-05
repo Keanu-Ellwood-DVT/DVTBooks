ec473e6a8401710f96ed76559c3560eac2ca8590
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-05-05 20:30:19 +0200

    Update app routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,27 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from '../profile/profile.component';
 import { AuthGuard } from '../auth.guard';
 import { ForbiddenComponent } from '../forbidden/forbidden.component';
 import { AdminComponent } from '../admin/admin.component';
 import { AdminGuard } from '../admin.guard';
 import { BookInfoComponent } from '../book-info/book-info.component';
 import { HomeComponent } from '../home/home.component';
+import { AuthenticationGuard } from '../authentication.guard';
+import { SearchResultsListComponent } from '../search-results-list/search-results-list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  },
   {
     path: 'home',
     component: HomeComponent
   },
   {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [AuthGuard]
+    path: 'results',
+    component: SearchResultsListComponent
   },
   {
     path: 'admin',
@@ -36,20 +31,12 @@ const routes: Routes = [
   },
   {
     path: 'book/:isbn',
-    component: BookInfoComponent,
-    canActivate: [AuthGuard]
+    component: BookInfoComponent
   },
   {
-    path: 'author',
-    component: ProfileComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: '',
-        canActivate: [AdminGuard],
-        component: AdminComponent
-      }
-    ]
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
   },
   {
     path: 'forbidden',
