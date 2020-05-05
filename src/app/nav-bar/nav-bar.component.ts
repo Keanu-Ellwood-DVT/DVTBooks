9bbb189c0da114600551b489940ea34e8468f1f9
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-05-05 20:31:14 +0200

    Add search bar + search functionality

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,37 @@ import { AuthService } from '../auth.service';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  query: string = '';
+
+  constructor(public auth: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateToAdmin() {
+    const authCheck = document.getElementById('AuthorCheck') as HTMLInputElement;
+    const bookCheck = document.getElementById('BookCheck') as HTMLInputElement;
+    //console.log(authCheck.checked + ' ' + bookCheck.checked + ' ' + this.query)
+    if (authCheck.checked) {
+      if (this.query) {
+        this.router.navigate([`/results`, { q: this.query, cat: authCheck.value }]);
+      } else {
+        this.router.navigate([`/results`, { cat: authCheck.value }]);
+      }
+    } else if (bookCheck.checked) {
+      if (this.query) {
+        this.router.navigate([`/results`, { q: this.query, cat: bookCheck.value }]);
+      } else {
+        this.router.navigate([`/results`, { cat: bookCheck.value }]);
+      }
+    } else {
+      if (this.query) {
+        this.router.navigate([`/results`, { q: this.query, cat: "All" }]);
+      }else {
+        this.router.navigate([`/results`, { cat: "All" }]);
+      }
+    }
   }
 
 }
