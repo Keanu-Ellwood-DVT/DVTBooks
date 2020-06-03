83e95d24c9034c9e5eae4bc43d128d66a0b505bc
Keanu Ellwood <kellwood@jhb.dvt.co.za>
2020-06-03 15:14:58 +0200

    Fix change detection issues

--- src/app/components/admin/new-author/new-author.component.ts
+++ src/app/components/admin/new-author/new-author.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { AuthorService } from 'src/app/shared/services/author.service';
 import { Author } from 'src/app/shared/models/author';
 import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
@@ -16,12 +16,13 @@ export class NewAuthorComponent implements OnInit {
   @Input()
   currentAuth?: Author;
   form: FormGroup;
-  newAuthor: Author;
+  newAuthor: Author = {} as Author;
 
   submitted = false;
 
   constructor(
     private authorService: AuthorService,
+    private cd: ChangeDetectorRef
   ) {
     this.form = new FormGroup({
       firstName: new FormControl('', { validators: [Validators.required] }),
@@ -37,37 +38,35 @@ export class NewAuthorComponent implements OnInit {
   get about(): AbstractControl { return this.form.get('about'); }
 
   ngOnInit(): void {
-
-    if (this.state === 'Update' && this.currentAuth) {
+    if (this.state === 'Update' && !!this.currentAuth) {
       this.authorService.getAuthor(this.currentAuth.id).subscribe(x => {
         this.newAuthor = x;
       });
     } else {
-      this.newAuthor = {
-        href: null,
-        id: null,
-        first_name: '',
-        middle_names: null,
-        last_name: '',
-        name: null,
-        about: '',
-        version: null,
-        books: null
-      };
+        this.newAuthor = {
+          href: null,
+          id: null,
+          first_name: '',
+          middle_names: null,
+          last_name: '',
+          name: null,
+          about: '',
+          version: null,
+          books: null
+        };
+        this.cd.detectChanges();
     }
   }
 
   addAuthor() {
     if (this.state !== 'Update') {
       this.authorService.putAuthor(this.newAuthor);
-      this.newAuthor.first_name = '';
-      this.newAuthor.middle_names = null;
-      this.newAuthor.last_name = '';
-      this.newAuthor.about = '';
-      this.submitted = true;
+        this.newAuthor.first_name = '';
+        this.newAuthor.middle_names = null;
+        this.newAuthor.last_name = '';
+        this.newAuthor.about = '';
     } else {
       this.authorService.putAuthor(this.newAuthor, this.newAuthor.id);
-      this.submitted = true;
     }
   }
 
